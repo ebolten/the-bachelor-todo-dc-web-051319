3bf5ed42b1efd7fba25190654d955491a9a35bcc
Emily Bolten <emilybolten@gmail.com>
2019-05-11 15:27:29 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -24,6 +24,25 @@ end
 
 def get_contestant_name(data, occupation)
   # code here
+  name = nil
+  data.each do |userSeason, info|
+      if userSeason.to_s == season
+        info.each do |element|
+          element.each do |key, value|
+            if key.to_s == "name"
+            name = value.to_s
+          end
+            if key.to_s == "occupation"
+              if value.to_s == occupation
+                index = name.index(" ")
+                name = name[0..index - 1]
+                return name
+              end
+            end
+          end
+        end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
