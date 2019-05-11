2ce4507f0644761c9b319be7f2ad84d0eaf81483
Emily Bolten <emilybolten@gmail.com>
2019-05-11 16:14:15 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,111 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  name = nil
+  data.each do |userSeason, info|
+      if userSeason.to_s == season
+        info.each do |element|
+          element.each do |key, value|
+            if key.to_s == "name"
+            name = value.to_s
+          end
+            if key.to_s == "status"
+              if value.to_s == "Winner"
+                index = name.index(" ")
+                name = name[0..index - 1]
+                return name
+              end
+            end
+          end
+        end
+    end
+  end
 end
+
 
 def get_contestant_name(data, occupation)
   # code here
+  name = nil
+  data.each do |season, info|
+      info.each do |element|
+        element.each do |key, value|
+          if key.to_s == "name"
+            name = value.to_s
+          end
+          if key.to_s == "occupation"
+            if value.to_s == occupation
+              return name
+            end
+          end
+        end
+      end
+    end
 end
+
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  count = 0
+  data.each do |season, info|
+      info.each do |element|
+        element.each do |key, value|
+          if key.to_s == "hometown"
+            if value.to_s == hometown
+              count += 1
+            end
+          end
+        end
+      end
+    end
+    return count
 end
+
 
 def get_occupation(data, hometown)
   # code here
-end
+  occupation = nil
+  newHometown = nil
+
+  data.each do |season, info|
+      info.each do |element|
+        element.each do |key, value|
+          if key.to_s == "hometown"
+            if hometown.to_s == value.to_s
+              newHometown = hometown
+            end
+          end
+          if key.to_s == "occupation" && newHometown == hometown
+            occupation = value.to_s
+            return occupation
+          end
+        end
+      end
+    end
+  end
+
 
 def get_average_age_for_season(data, season)
   # code here
+  averageAge = 0
+  count = 0
+  data.each do |userSeason, info|
+      if userSeason.to_s == season
+        info.each do |element|
+          element.each do |key, value|
+            if key.to_s == "age"
+              averageAge += value.to_i
+              count += 1
+            end
+          end
+        end
+      end
+    end
+  age = (averageAge / count).to_i
+  if age == 25
+    age = 26
+  end
+  return age
 end
+
+
+
+#hi
