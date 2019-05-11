992144d48082e461e8ae718e505aa8c17ebf5948
Emily Bolten <emilybolten@gmail.com>
2019-05-11 15:12:47 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,22 @@
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season].each do |info|
-      if data[season] == "Winner"
-        name = data[season]["name"]
-        return name
+  name = nil
+  data.each do |userSeason, info|
+      if userSeason.to_s == season
+        info.each do |element|
+          element.each do |key, value|
+            if key.to_s == "name"
+            name = value.to_s
+          end
+            if key.to_s == "status"
+              if value.to_s == "Winner"
+                index = name.index(" ")
+                name = name[0..index]
+                return name
+              end
+            end
+          end
+        end
     end
   end
 end
