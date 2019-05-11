73210c9eb1f9de80f997727da500ac233650daf2
Emily Bolten <emilybolten@gmail.com>
2019-05-11 14:39:46 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,16 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |name, data|
+    name.each do |info|
+      if data[season][name]["status"] == "Winner"
+        index = name.index(" ")
+        name = name[0..index]
+        return name
+      end
+    end
+  end
+
+
 end
 
 def get_contestant_name(data, occupation)
